a15506f7b6bed50a6408f8dbb518807cd761f4a9
jtzhao <13020590959@163.com>
2025-02-22 00:46:19 +0800

    in my laptop

--- code/B_tree.py
+++ code/B_tree.py
@@ -1,6 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import os
+from scipy.stats import expon
+from scipy.stats import kstest
 
 def random_block(L,gridll):
     """
@@ -120,13 +122,15 @@ def plot_score_distribution_and_save(score_list, L):
     绘制得分的频率分布直方图，并进行归一化
     """
     plt.figure(figsize=(10, 6))  
-    n, bins, patches = plt.hist(score_list, bins=150, 
-                                density=True, facecolor='g', alpha=0.75)  
+    wight_list = np.ones_like(score_list)/float(len(score_list))
+    n, bins, patches = plt.hist(score_list, bins=1200, 
+                                density=False, weights=wight_list, facecolor='g', alpha=0.75)  
    
     
     plt.xlabel('Score',fontsize=18)
     plt.ylabel('Frequency', fontsize=18) 
-    plt.xlim(0,max(score_list)) 
+    plt.xlim(0,40) 
+    plt.ylim(0,0.8)
     plt.title(f'Score Distribution (L={L})',fontsize=25) 
     plt.xticks(fontsize=18)
     plt.yticks(fontsize=18)
@@ -136,6 +140,26 @@ def plot_score_distribution_and_save(score_list, L):
     plt.savefig(save_path) 
     plt.show() 
    
+# def plot_score_distribution_for_n_L(L_list, total_time):
+#     score_list_for_L = {}
+#     for L in L_list:
+#         _, score_list, _ = main(total_time, L)  # 提取得分列表
+#         score_list_for_L[f"score_{L}"] = score_list
+#         plt.hist(score_list_for_L[f"score_{L}"], bins=3000,
+#                  alpha=0.5, label=f"L = {L}", density=True)
+
+#     plt.xlabel('Score')
+#     plt.ylabel('Frequency')
+#     plt.title('Score Distribution of multiple L')
+#     plt.xlim(0, 50)
+#     plt.legend()
+#     plt.grid(True)
+
+#     save_path = f"./figure/B_for_n_L.png"
+#     os.makedirs(os.path.dirname(save_path), exist_ok=True)
+#     plt.savefig(save_path)
+#     plt.show()
+
 
 def main_1(average_density):
     """
@@ -149,20 +173,37 @@ def main_1(average_density):
     plt.yticks(fontsize=18)
     plt.show()
 
+
+# # 生成图像
+# if __name__ == '__main__':
+#     # L = 30
+#     # total_time = 16000
+#     # gridll,score_list,average_density = main(total_time,L)
+#     # main_1(average_density)
+#     # # xmax = {5:20,10:35,15:70,20:100,25:200,30:200,32:200,35:250,40:250}
+#     # plot_score_distribution(score_list, L)
+#     L_list = [30,35,40,45,32]
+#     # L_list = [32]
+#     # print(f"L={L},the maximum score is:",max(score_list))
+#     for L in L_list:
+#         total_time = 16000
+#         gridll,score_list,average_density = main(total_time,L)
+#         print(f"L={L},the maximum score is:",max(score_list))
+#         plot_score_distribution_and_save(score_list, L)
+
+
+# if __name__ == '__main__':
+#     L_list= [10,20,30,40,50]
+#     total_time = 8000
+#     plot_score_distribution_for_n_L(L_list,total_time)
+
+# 验证指数分布
 if __name__ == '__main__':
     L = 32
-    total_time = 8000
+    total_time = 16000
     gridll,score_list,average_density = main(total_time,L)
-    # main_1(average_density)
-    plot_score_distribution(score_list, L)
-    L_list = [5,10,15,20,25,30,35,40]
-    print(f"L={L},the maximum score is:",max(score_list))
-    for L in L_list:
-        total_time = 8000
-        gridll,score_list,average_density = main(total_time,L)
-        print(f"L={L},the maximum score is:",max(score_list))
-        plot_score_distribution_and_save(score_list, L)
-
-    
-
-
+    # 验证指数分布
+    data = np.array(score_list)
+    df = 4
+    result = kstest(data, 'chi2',args = (df,))
+    print(result)
